c17b205b1a9be3f0509b249a276d4c275b4943a0
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-02-25 12:44:17 -0600

    adding main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+  Angel Odiel Treviño Villanueva
+  A01336559
+  Tarea: Tar08-Audiophobia Fecha: 25/feb  Materia: Diseño de Algoritmos
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  /* Code Here */
+
+  return 0;
+}
