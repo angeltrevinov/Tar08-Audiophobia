18b92f21cc89d8e5285bb3c5e8a2a2eff36aa2d1
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-02-25 22:53:07 -0600

    done

--- main.cpp
+++ main.cpp
@@ -14,24 +14,24 @@ void AlgoritmoFloyd(int D[100][100], int C){
   for(int k = 0; k < C; k++){
     for(int i = 0; i < C; i++){
       for(int j = 0; j < C; j++){
-        if(D[i][k] > D[k][j] && D[i][k] <= D[i][j]){
-          D[i][j] = D[i][k];
-        }else if(D[k][j] > D[i][k] && D[k][j] <= D[i][j]){
-          D[i][j] = D[k][j];
+        int Max;
+        if(D[i][k] > D[k][j]){
+            Max = D[i][k];
         }else{
-          D[i][j] = D[i][k] = D[k][j];
+            Max = D[k][j];
+        }
+        if(Max < D[i][j]){
+            D[i][j] = Max;
         }
       }
     }
   }
 }
 
-
 int main(){
 
   int cases; //number of cases
   vector<int> Respuestas;
-  bool range = true;
   cin >> cases;
   for(int z = 0; z < cases; z++){
     int C; //cantidad de crossings o nodos
@@ -58,30 +58,27 @@ int main(){
     AlgoritmoFloyd(D, C);
     for(int i = 0; i < Q; i++){
       cin >> a >> b;
-      if(a <= 0 || b <= 0 || a > C || b > C){//por si se sale afuera del rango
-        range = false;
-      }
       //para imprimir respuestas
-      if(D[a-1][b-1] != INT_MAX && range){
+      if(D[a-1][b-1] != INT_MAX){
         Respuestas.push_back(D[a-1][b-1]);
       }else{
         Respuestas.push_back(INT_MAX);
       }
-      Respuestas.push_back(0);
     }
+      Respuestas.push_back(0);
   }
   //aqui las imprimo
-  int iE = 0;
+  int j = 0;
     for(int i = 0; i < cases; i++){
         cout << "Case " << i+1 << ":"<< endl;
-        while(Respuestas[iE] != 0){
-            if(Respuestas[iE] != INT_MAX)
-                cout << Respuestas[iE] << endl;
+        while(Respuestas[j] != 0){
+            if(Respuestas[j] != INT_MAX)
+                cout << Respuestas[j] << endl;
             else
                 cout << "no path" << endl;
-            iE++;
+            j++;
         }
-        iE++;
+        j++;
     }
   return 0;
 }
